cfa8a22ba4f38332c138628825b1fb9cf4b1461e
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2025-10-23 12:47:49 -0400

    Updated cleaner.py to handle multiple data files and continued setting up the database connection in db.py

--- cleaner.py
+++ cleaner.py
@@ -1,66 +1,51 @@
-import pandas
+import pandas, os
+from pandas.api.types import is_numeric_dtype
 
-raw = pandas.read_csv('biscayne_bay_dataset_oct_2022.csv')
+data_files = os.listdir("data")
 
-print(raw)
+result = {}
 
-print("Number of rows originally: " + str(len(raw)) + "\n")
+for file in data_files:
+    file_path = os.path.join("data", file)
 
-lat_data = raw['latitude']
-lon_data = raw['longitude']
-column_data = raw['Total Water Column (m)']
-temp_data = raw['Temperature (C)']
-ph_data = raw['pH']
-odo_data = raw['ODO (mg/L)']
+    df = pandas.read_csv(file_path)
+    clean = df
 
-lat_mean = lat_data.mean()
-lat_sd = lat_data.std()
+    columns = df.columns.to_list()
 
-lon_mean = lon_data.mean()
-lon_sd = lon_data.std()
-
-column_mean = lon_data.mean()
-column_sd = column_data.std()
-
-temp_mean = temp_data.mean()
-temp_sd = temp_data.std()
-
-ph_mean = ph_data.mean()
-ph_sd = ph_data.std()
-
-odo_mean = odo_data.mean()
-odo_sd = odo_data.std()
-
-clean = raw
-
-dropped_rows = 0
-
-def zfactor(x, mean, std):
-    return abs((x - mean) / std)
-
-for i in range(0, len(raw)):
-    try:
-        row = clean.iloc[i]
-    except Exception:
-        break
-    
-    lat = row['latitude']
-    lon = row['longitude']
-    column = row['Total Water Column (m)']
-    temp = row['Temperature (C)']
-    odo = row['ODO (mg/L)'] 
-    ph = row['pH']
-
-    lat_z = zfactor(lat, lat_mean, lat_sd)
-    lon_z = zfactor(lon, lon_mean, lon_sd)
-    column_z = zfactor(column, column_mean, column_sd)
-    temp_z = zfactor(temp, temp_mean, temp_sd)
-    odo_z = zfactor(odo, odo_mean, odo_sd)
-    ph_z = zfactor(ph, ph_mean, ph_sd)
+    numeric_columns = [column for column in columns if is_numeric_dtype(df[column])]
 
 
-    if lat_z > 3 or lon_z > 3 or column_z > 3 or temp_z > 3 or odo_z > 3 or ph_z > 3:
-        clean = clean.drop(i)
-        i -= 1
+    mean_dict = {}
+    std_dict = {}
 
-print("Number of rows dropped: " + str(len(clean)))
+
+    for column in numeric_columns:
+        mean_dict[column] = float(df[column].mean())
+        std_dict[column] = float(df[column].std())
+
+        if std_dict[column] == 0.0:
+            numeric_columns.remove(column)
+
+    print(numeric_columns)
+
+    for i in range(0, len(df)):
+        try:
+            row = df.iloc[i]
+        except Exception:
+            break
+
+        for column in numeric_columns:
+            value = row[column]
+            try:
+                zscore = (value - mean_dict[column]) / std_dict[column]
+            except Exception:
+                continue
+
+            if abs(zscore) > 3:
+                clean.drop(i)
+                break
+
+    print(f"Dropped {len(df) - len(clean)} rows from {file}")
+    result[file] = clean
+
--- db.py
+++ db.py
@@ -1,7 +1,7 @@
-import os
+import os, json
 from pymongo import MongoClient
 from dotenv import load_dotenv
-from cleaner import clean
+from cleaner import result
 
 load_dotenv()
 
@@ -9,7 +9,11 @@ mongo_url = os.getenv("MONGO_URL")
 
 client = MongoClient(mongo_url)
 
-try:
-    client.bulk_write(clean)
-except:
-    print("Error running MongoClient")
+db = client.get_database("water_data")
+
+
+for filename in result:
+    collection = db.get_collection(filename)
+    data = result[filename].to_dict('records')
+    collection.insert_many(data)
+
